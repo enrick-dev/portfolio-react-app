9570ac83bbf3351436da72aaf8f994c6563651f9
Enrick Santos de Souza <enricksantos03@gmail.com>
2022-05-07 22:06:32 -0300

    Home Completa

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-2xl font-bold">Hello</h1>
+      <Navbar/>
+      <Home/>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons/hi'
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+
+        {/* Container */}
+        <div className='max-w-[1000px] h-full mx-auto px-8 flex flex-col justify-center'>
+            <p className='text-[#DB2777]'>Olá, meu nome é</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Enrick Santos</h1>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Eu sou um desenvolvedor Front End.</h2>
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>Sou um jovem sem experiencia profissional, mas com experiência em 
+            alguns pequenos projetos pessoais. Tenho habilidades no desenvolvimento 
+            web em Html, Css, Javascript, PHP e MySql. Atualmente estudando React 
+            Js e React Native.</p>
+            <div>
+                <button className='group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-[#DB2777] hover:border-[#DB2777]'>
+                Ver Trabalho <HiArrowNarrowRight className='ml-3 group-hover:rotate-90 duration-300'/> </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
