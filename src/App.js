986aa863c9e1dfdc8c6f96f31674fc0204f198f0
Enrick Santos de Souza <enricksantos03@gmail.com>
2022-07-12 18:13:17 -0300

    Contact

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
@@ -13,6 +14,7 @@ function App() {
       <About/>
       <Skills/>
       <Work/>
+      <Contact/>
     </div>
   );
 }
