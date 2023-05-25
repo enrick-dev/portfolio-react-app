775cacebfbaf185d439923c8764b1e20fb87c6bf
Enrick Santos <65925717+rick-png@users.noreply.github.com>
2023-05-25 19:53:50 -0300

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -16,8 +16,7 @@ const Work = () => {
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
                 
                 {/* Grid */}
-                <div style={{backgroundImage: `url(${WorkImg1})`}}
-                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                <div style={{backgroundImage: `url(${WorkImg1})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* Efeito de Hover */}
                     <div className='opacity-0 group-hover:opacity-100'>
@@ -34,6 +33,25 @@ const Work = () => {
                             </a>
                         </div>
                     </div>
+                    
+                    <div style={{backgroundImage: `url(${WorkImg1})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                    {/* Efeito de Hover */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider flex text-center'>
+                        Aplicação em <br/> React JS e Tailwindcss
+                        </span>
+                        <div className='flex justify-center'><p>Em desenvolvimento</p></div>
+                        <div className='pt-8 text-center'>
+                            <a href="https://smartbanking-landingpage-figmadesign.vercel.app">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Site</button>
+                            </a>
+                            <a href="https://github.com/rick-png/smartbanking-landingpage-figmadesign">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                    
                 </div>
                 
                 
